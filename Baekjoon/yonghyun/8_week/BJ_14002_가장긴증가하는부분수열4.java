86796c5bbe752fe1bf5d7eb7ee55b54b3f025d4e
byh9811 <byh9811@naver.com>
2023-03-11 22:28:46 +0900

    임시저장

---
+++ Baekjoon/yonghyun/8_week/BJ_14002_가장긴증가하는부분수열4.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+import static java.lang.Integer.parseInt;
+
+/**
+ * 가장 긴 증가하는 부분 수열 4
+ * https://www.acmicpc.net/problem/14002
+ *
+ * 1.
+ *
+ * @author 배용현
+ *
+ */
+public class BJ_14002_가장긴증가하는부분수열4 {
+
+    static int N, M, H, answer;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static boolean[] ladder;        // 사다리가 존재하는지 정보를 담은 배열
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(answer);
+    }
+}
