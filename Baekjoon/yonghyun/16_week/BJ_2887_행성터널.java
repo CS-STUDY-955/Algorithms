d5d1cab13c6e3e3dad7671a0d4cae934af77122f
byh9811 <byh9811@naver.com>
2023-05-10 02:27:09 +0900

    temp

---
+++ Baekjoon/yonghyun/16_week/BJ_2887_행성터널.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+import static java.lang.Integer.parseInt;
+
+/**
+ * 행성 터널
+ * https://www.acmicpc.net/problem/2887
+ *
+ * 1. 얼핏 생각하면 외판원 순회와 비슷한 문제인 것 같지만, N이 최대 100,000이기 때문에 불가능하다.
+ * 2. 모든 노드를 연결하는 가장 짧은 경로를 구하는 문제인데 외판원 순회와 다른 점을 찾아야한다.
+ * 3. 거리가 아닌 좌표가 주어졌다는 점이 다르므로 이를 이용해야 한다.
+ * 4.
+ *
+ * @author 배용현
+ *
+ */
+public class BJ_2887_행성터널 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        int N = parseInt(br.readLine());        // 행성의 수
+
+    }
+
+}
