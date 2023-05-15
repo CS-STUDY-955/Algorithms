b3d2648f93358a7092d8489553e591427ff949b0
byh9811 <byh9811@naver.com>
2023-05-16 01:07:38 +0900

    [BJ_1865] 웜홀

--- Baekjoon/yonghyun/17_week/BJ_1865_웜홀.java
+++ Baekjoon/yonghyun/17_week/BJ_1865_웜홀.java
@@ -2,8 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 import static java.lang.Integer.parseInt;
